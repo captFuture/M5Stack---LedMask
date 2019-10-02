5b466055c6f0e0ce72022e33f246067309f2b476
Chris <profsimon@gmail.com>
2019-10-02 18:51:41 +0200

    Moved background from spiffs to image array

--- src/RGBmask.ino
+++ src/RGBmask.ino
@@ -1,6 +1,6 @@
 #include "variables.h"
 #include "FS.h"
-#include "SPIFFS.h"
+#include "backgroundimage.h"
 #include <M5Stack.h>
 #include <FastLED.h>
 CRGB leds1[NUM_LEDS1];
@@ -20,9 +20,6 @@ CRGB leds1[NUM_LEDS1];
 #include <math.h>
 #include "vumeter.h"
 
-#define imgTml tmllogo
-#define PicArray extern unsigned char
-PicArray imgTml[];
 
 void setup() {
   M5.begin();
@@ -35,12 +32,7 @@ void setup() {
   FastLED.setBrightness( scale8(minBrightness, maxBrightness) );
 
   M5.Lcd.fillScreen(TFT_BLACK);
-  if(!SPIFFS.begin(true)){
-    Serial.println("SPIFFS Mount Failed");
-  }else{
-    Serial.println("SPIFFS Mount Success");
-    M5.Lcd.drawJpgFile(SPIFFS, "/tml_logo.jpg", 0, 0);
-  }
+
   M5.Lcd.setTextColor(YELLOW);
   M5.Lcd.setTextSize(2);
   M5.Lcd.setCursor(10, 10);
@@ -139,7 +131,9 @@ void loop()
     if(enableMusic > 2){enableMusic = 0;};
 
     M5.Lcd.fillScreen(TFT_BLACK);
-    M5.Lcd.drawJpgFile(SPIFFS, "/tml_logo.jpg", 0, 0);
+
+    M5.Lcd.drawJpg(bg_image, 153600, 0, 0);
+
     M5.Lcd.setTextColor(YELLOW);
     M5.Lcd.setTextSize(2);
     M5.Lcd.setCursor(30, 10);
